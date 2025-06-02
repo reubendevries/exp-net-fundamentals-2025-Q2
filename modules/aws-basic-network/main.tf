6e5d705066ca421e38231346674f6ee8af7cd050
Reuben deVries <reubendevries@gmail.com>
2025-06-02 14:24:25 -0700

    Fix .gitignore; remove tfvars from gitignore file. Add variables to terraform.tfvars files. Fix subnetting so it creates three subnets. Fixes #4

--- modules/aws-basic-network/main.tf
+++ modules/aws-basic-network/main.tf
@@ -4,13 +4,30 @@ resource "aws_vpc" "network_fundamentals_vpc" {
     enable_dns_hostnames = true
     assign_generated_ipv6_cidr_block = true
     instance_tenancy = "default"
-    tags = merge({ "Name" = format("%s-vpc", local.environment_name)}, local.tags)
+    tags = merge({ "Name" = format("%s VPC", local.environment_name)}, local.tags)
 }
 
 resource "aws_subnet" "public_subnet" {
-    count = local.count
+    for_each = local.public_subnets
+    
     vpc_id = aws_vpc.network_fundamentals_vpc.id
-    cidr_block = local.public_subnet_cidr
-    availability_zone = local.availability_zone
-    tags = merge({ "Name" = format("%s-public-subnet", local.environment_name )}, local.tags)
+    cidr_block = each.value
+    availability_zone = each.key
+    map_public_ip_on_launch = true
+    
+    tags = merge({ 
+        "Name" = format("%s Public Subnet - %s", local.environment_name, each.key)
+    }, local.tags)
+}
+
+resource "aws_subnet" "private_subnet" {
+    for_each = local.private_subnets
+    
+    vpc_id = aws_vpc.network_fundamentals_vpc.id
+    cidr_block = each.value
+    availability_zone = each.key
+    
+    tags = merge({ 
+        "Name" = format("%s Private Subnet - %s", local.environment_name, each.key)
+    }, local.tags)
 }
---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+availability_zone = ["ca-central-1a", "ca-central-1b", "ca-central-1c"]
+aws_region  = "ca-central-1"
+environment_name = "Networking Fundatamental Bootcamp"
+private_subnet_cidr = ["10.10.21.0/24", "10.10.22.0/24", "10.10.23.0/24"]
+public_subnet_cidr = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
+tags = {
+    "Owner" = "ExamPro.co",
+    "BoundedContext" = "Network Fundatmentals Bootcamp"
+    "ManagedBy" = "Terraform"
+    "Environment" = "Staging"
+    "Region" = "ca-central-1"
+}
+vpc_cidr  = "10.0.0.0/16"
