121157b95cd250a19ac5cb6c0cdf7f61965b0672
Reuben deVries <reubendevries@gmail.com>
2025-06-02 21:12:11 -0700

    ran terraform fmt

--- modules/aws-basic-network/locals.tf
+++ modules/aws-basic-network/locals.tf
@@ -1,16 +1,16 @@
 locals {
-    aws_region = var.aws_region
-    environment_name = var.environment_name
-    private_subnets = {
-        "${var.availability_zone[0]}" = var.private_subnet_cidr[0]
-        "${var.availability_zone[1]}" = var.private_subnet_cidr[1]
-        "${var.availability_zone[2]}" = var.private_subnet_cidr[2]
-    }
-    public_subnets = {
-        "${var.availability_zone[0]}" = var.public_subnet_cidr[0]
-        "${var.availability_zone[1]}" = var.public_subnet_cidr[1]
-        "${var.availability_zone[2]}" = var.public_subnet_cidr[2]
-    }
-    tags = var.tags
-    vpc_cidr = var.vpc_cidr
+  aws_region       = var.aws_region
+  environment_name = var.environment_name
+  private_subnets = {
+    "${var.availability_zone[0]}" = var.private_subnet_cidr[0]
+    "${var.availability_zone[1]}" = var.private_subnet_cidr[1]
+    "${var.availability_zone[2]}" = var.private_subnet_cidr[2]
+  }
+  public_subnets = {
+    "${var.availability_zone[0]}" = var.public_subnet_cidr[0]
+    "${var.availability_zone[1]}" = var.public_subnet_cidr[1]
+    "${var.availability_zone[2]}" = var.public_subnet_cidr[2]
+  }
+  tags     = var.tags
+  vpc_cidr = var.vpc_cidr
 }
--- modules/aws-basic-network/main.tf
+++ modules/aws-basic-network/main.tf
@@ -1,33 +1,33 @@
 resource "aws_vpc" "network_fundamentals_vpc" {
-    cidr_block = local.vpc_cidr
-    enable_dns_support = true
-    enable_dns_hostnames = true
-    assign_generated_ipv6_cidr_block = true
-    instance_tenancy = "default"
-    tags = merge({ "Name" = format("%s VPC", local.environment_name)}, local.tags)
+  cidr_block                       = local.vpc_cidr
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  assign_generated_ipv6_cidr_block = true
+  instance_tenancy                 = "default"
+  tags                             = merge({ "Name" = format("%s VPC", local.environment_name) }, local.tags)
 }
 
 resource "aws_subnet" "public_subnet" {
-    for_each = local.public_subnets
-    
-    vpc_id = aws_vpc.network_fundamentals_vpc.id
-    cidr_block = each.value
-    availability_zone = each.key
-    map_public_ip_on_launch = true
-    
-    tags = merge({ 
-        "Name" = format("%s Public Subnet - %s", local.environment_name, each.key)
-    }, local.tags)
+  for_each = local.public_subnets
+
+  vpc_id                  = aws_vpc.network_fundamentals_vpc.id
+  cidr_block              = each.value
+  availability_zone       = each.key
+  map_public_ip_on_launch = true
+
+  tags = merge({
+    "Name" = format("%s Public Subnet - %s", local.environment_name, each.key)
+  }, local.tags)
 }
 
 resource "aws_subnet" "private_subnet" {
-    for_each = local.private_subnets
-    
-    vpc_id = aws_vpc.network_fundamentals_vpc.id
-    cidr_block = each.value
-    availability_zone = each.key
-    
-    tags = merge({ 
-        "Name" = format("%s Private Subnet - %s", local.environment_name, each.key)
-    }, local.tags)
+  for_each = local.private_subnets
+
+  vpc_id            = aws_vpc.network_fundamentals_vpc.id
+  cidr_block        = each.value
+  availability_zone = each.key
+
+  tags = merge({
+    "Name" = format("%s Private Subnet - %s", local.environment_name, each.key)
+  }, local.tags)
 }
